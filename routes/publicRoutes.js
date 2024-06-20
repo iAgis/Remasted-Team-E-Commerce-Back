b690080b791d3febe8a524a2eab8b2547ba1d79f
Agustin Lemes <86081166+iAgis@users.noreply.github.com>
2024-06-20 00:10:08 -0300

    Update publicRoutes.js

--- routes/publicRoutes.js
+++ routes/publicRoutes.js
@@ -4,8 +4,8 @@ const { testDB } = require("../models");
 
 publicRouter.get("/public", (req, res) => res.json("Hola C:"));
 publicRouter.get("/db", async (req, res) => {
-  const res = await testDB();
-  res.json(res);
+  const resTest = await testDB();
+  res.json(resTest);
 });
 
 module.exports = publicRouter;
